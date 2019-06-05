a012062658ec653686ea4579ac3050e3ca453b18
Sitan Huang(黄斯坦) <978494543.qq54@gmail.com>
2019-06-04 22:07:43 -0700

    Add files via upload

--- data.js
+++ data.js
@@ -17,7 +17,7 @@ AMP_MAX = 1;
 90	22	80.9	14	18.9	235	-7.2	113	23	16	0.6956521739				0	0	0	0	1	0
 95	22	79	0	18	179	-7.9	94	18	11	0.6111111111				1	0	0	0	0	0
 84	407c	12.8	3.7	68	211	43	104	18	11	0.6111111111				0	0	1	1	0	0
-78	22	2.5	9.9	63	180	39.9	94.6	19.2	12	0.625				0	0.2	0	0	0	0
+78	22	2.5	9.9	63	180	39.9	94.6	19.2	12	0.625				0	0.2	0.2	0.2	0	0.2
 92	22	55.5	3.8	42	249	23.8	115.6			#DIV/0!				1	0	0	0	0	0
 85	22	19	13	70	255	44.6	117.3			#DIV/0!				0	0	0	0	0	0
 97	410a	11	36.3	134.4	489	46	131.9	16	12.6	0.7875				0	1	0	0	0	0
@@ -55,12 +55,17 @@ AMP_MAX = 1;
              output: [underCharge, overCharge, lowIndoorFlow, dirtyCoil, restriction, txvStuckOpen]});
 });
 
-function test(ambient, sh, sc, lt, ht, designAmp, actualAmp) {
+function activate(ambient, sh, sc, lt, ht, designAmp, actualAmp) {
   let results = network.activate([normalize(ambient, AMBIENT_MAX),
                   normalize(sh, SH_MAX),
                   normalize(sc, SC_MAX),
                   normalize(lt, LT_MAX),
                   normalize(ht, HT_MAX)]).map(x => (Math.round(x * 100)));
+  return results;
+}
+
+function test(ambient, sh, sc, lt, ht, designAmp, actualAmp) {
+  let results = activate(ambient, sh, sc, lt, ht, designAmp, actualAmp);
   return `Undercharge: ${results[0]}%, Overcharge: ${results[1]}%,
   Low Indoor Flow: ${results[2]}%, Dirty coil: ${results[3]}%,
   Restriction: ${results[4]}%, TXV Stuck Open: ${results[5]}%`;
--- index.js
+++ index.js
@@ -33,3 +33,4 @@ function evolve() {
 
 
 prep();
+evolve();
